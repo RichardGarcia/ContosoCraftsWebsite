2e745beee565226c8f9ec54f4fcc340e18c78026
Richard Garcia <richardgarciaph@gmail.com>
2021-10-01 17:24:41 +1300

    add LINQ syntax???

--- ContosoCraftWebsite/Models/Product.cs
+++ ContosoCraftWebsite/Models/Product.cs
@@ -1,19 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace ContosoCraftWebsite.Models
 {
     public class Product
     {
-        public int Id { get; set; }
+        [JsonPropertyName("img")]
+        public string Id { get; set; }
         public string Maker { get; set; }
         public string Image { get; set; }
         public string Url { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public int[] Ratings { get; set; }
+        public override string ToString() => JsonSerializer.Serialize<Product>(this);
 
     }
+
+
 }
